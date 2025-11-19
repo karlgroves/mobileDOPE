d43ec7b4504032babb1b82b03b4943f2903df16b
Karl Groves <karlgroves@gmail.com>
2025-11-18 20:17:31 -0500

    Fix missing Text import in RifleProfileList
    - Add Text import from react-native
    - Fixes 'Cannot call a class as a function' error

--- src/screens/RifleProfileList.tsx
+++ src/screens/RifleProfileList.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, FlatList, StyleSheet, Alert } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import {
